54e26b207193bf845ec6229fcdf48bec10a00f12
hso <sh07202@naver.com>
2024-05-20 17:43:40 +0900

    feat(Post) : 포스트 생성 기본 기능 구현
    추후 새로생성/수정 구분하여 함수 적용할 예정

---
+++ src/components/post/CreateNewPost.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <p id="title"> 위키 작성 </p>
+        <div>
+            <div id="save-btn">저장</div>
+        </div>
+    </div>
+        <form @submit.prevent="submitPost">
+      <div>
+        <label for="title">제목:</label>
+        <input type="text" id="title" v-model="postForm.title">
+      </div>
+      <div>
+        <label for="hashtags">해시태그 (쉼표로 구분):</label>
+        <input type="text" id="hashtags" v-model="postForm.tags">
+      </div>
+      <div>
+        <label for="content">내용(html 형식으로 작성):</label>
+        <textarea id="content" v-model="postForm.content"></textarea>
+      </div>
+      <button type="submit" @click="savePost">게시글 작성</button>
+    </form>
+    <div id="ai-chat">
+        검증/검색/맞춤법 등등
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+const tabId = useRoute().params.id;
+console.log(tabId);
+
+const savePost = () =>{
+    console.log(postForm.value);
+}
+
+const postForm = ref({
+    title: '',
+    content: '',
+    tags: []
+})
+</script>
+
+<style>
+
+</style>
