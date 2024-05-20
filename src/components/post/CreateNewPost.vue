d423a2c9c80fe017bbaee78aae5c701c355d4ee2
hso <sh07202@naver.com>
2024-05-20 17:46:52 +0900

    style(Post) : 중복 버튼 하나로 수정

--- src/components/post/CreateNewPost.vue
+++ src/components/post/CreateNewPost.vue
@@ -1,27 +1,27 @@
 <template>
-    <div>
-        <p id="title"> 위키 작성 </p>
+    <form @submit.prevent="submitPost">
         <div>
-            <div id="save-btn">저장</div>
+            <p id="title"> 위키 작성 </p>
+            <div>
+                <button id="save-btn" type="submit" @click="savePost">게시글 작성</button>
+            </div>
         </div>
-    </div>
-        <form @submit.prevent="submitPost">
-      <div>
-        <label for="title">제목:</label>
-        <input type="text" id="title" v-model="postForm.title">
-      </div>
-      <div>
-        <label for="hashtags">해시태그 (쉼표로 구분):</label>
-        <input type="text" id="hashtags" v-model="postForm.tags">
-      </div>
-      <div>
-        <label for="content">내용(html 형식으로 작성):</label>
-        <textarea id="content" v-model="postForm.content"></textarea>
-      </div>
-      <button type="submit" @click="savePost">게시글 작성</button>
+        <div>
+            <label for="title">제목:</label>
+            <input type="text" id="title" v-model="postForm.title">
+        </div>
+        <div>
+            <label for="hashtags">해시태그 (쉼표로 구분):</label>
+            <input type="text" id="hashtags" v-model="postForm.tags">
+        </div>
+        <div>
+            <label for="content">내용(html 형식으로 작성):</label>
+            <textarea id="content" v-model="postForm.content"></textarea>
+        </div>
     </form>
     <div id="ai-chat">
-        검증/검색/맞춤법 등등
+        <p>AI chat</p>
+        <div>검증/검색/맞춤법 등등</div>
     </div>
 </template>
 
@@ -32,7 +32,7 @@ import { useRouter, useRoute } from 'vue-router';
 const tabId = useRoute().params.id;
 console.log(tabId);
 
-const savePost = () =>{
+const savePost = () => {
     console.log(postForm.value);
 }
 
@@ -43,6 +43,4 @@ const postForm = ref({
 })
 </script>
 
-<style>
-
-</style>
+<style></style>
