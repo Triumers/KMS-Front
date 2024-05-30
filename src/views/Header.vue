bcefe44ec299ec1d4c7d089531f8fdb8af9c6f23
Leegiyeon <rldus3512@naver.com>
2024-05-30 20:55:11 +0900

    feat(header): 로고 이미지 추가

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,5 +1,9 @@
 <template>
- 헤더
+    <div class="header-left">
+      <div class="logo">
+        <img src="@/assets/images/logo_header.png" alt="Logo" @click="navigateTo('')" />
+      </div>
+    </div>
 </template>
 
 <script setup>
@@ -8,5 +12,30 @@ import { useRouter } from 'vue-router';
 </script>
 
 <style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  /* background-color: #F4F3F6; */
+  color: #000;
+  width: 100%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.header-left,
+.header-right {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.logo > img {
+  width: 200px;
+}
 </style>
 
