7ca77f52687ec090da5d6974d21f293d359fca0b
hso <sh07202@naver.com>
2024-05-31 18:13:28 +0900

    feat(post) : 전체 검색 기능 구현

---
+++ src/components/post/SearchAllPostList.vue
@@ -0,0 +1,163 @@
+<template>
+    <div id="top">
+        <h3 id="tab-name">검색 결과</h3>
+    </div>
+
+    <hr>
+
+    <div class="postList-div">
+        <div class="row row-cols-1 row-cols-1 row-cols-md-2">
+            <div class="col" v-for="post in postList" :key="post.id"
+                @click="postDetail(post.originId ? post.originId : post.id)">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="top-info">
+                            <b-avatar variant="info" size="4rem" id="profile-img"
+                                :src="post.author.profileImg ? post.author.profileImg : '@/assets/images/profile_image.png'"></b-avatar>
+                            <div id="author-date">
+                                <h5 class="author"> {{ post.author.name }} </h5>
+                                <p class="date"><small class="text-muted"> {{ convertToDate(post.createdAt) }}</small>
+                                </p>
+                            </div>
+                        </div>
+                        <h5 class="card-title"><strong>{{ post.title }}</strong></h5>
+                        <div class="content-preview">{{ post.content }}</div>
+                        <b-card-img :src="post.postImg" rounded alt="Image" bottom></b-card-img>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="like">
+                                <span class="material-icons">favorite</span>
+                                {{ post.likeList.length }}
+                            </p>
+                            <p class="tags">
+                                <span class="tag" v-for="tag in post.tags" :key="tag.id">
+                                    <b-badge>#{{ tag }}</b-badge>
+                                </span>&nbsp;
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import axios from 'axios';
+
+const router = useRouter();
+const currentRoute = useRoute();
+
+const postList = ref(null);
+const search = ref({
+    type: currentRoute.query.type,
+    keyword: currentRoute.query.keyword,
+    title: null,
+    content: null,
+    tags: []
+});
+
+onMounted(() => {
+    searchPost();
+});
+
+const postDetail = (postId) => {
+    router.push({
+        path: `${currentRoute.path}/detail/${postId}`
+    });
+};
+
+async function searchPost() {
+
+    search.value.title = null;
+    search.value.content = null;
+
+    switch (search.value.type) {
+        case 'title':
+            search.value.title = search.value.keyword;
+            break;
+        case 'keyword':
+            search.value.content = search.value.keyword;
+            break;
+    }
+
+    if (search.value.type == "tag")
+        search.value.tags = currentRoute.query.tags ? currentRoute.query.tags.split(',') : [];
+
+    await getPostList();
+}
+
+async function getPostList() {
+    try {
+        const response = await axios.post('http://localhost:5000/post/tab', {
+            tabRelationId: search.value.tabRelationId,
+            categoryId: search.value.categoryId,
+            title: search.value.title,
+            content: search.value.content,
+            tags: search.value.tags
+        });
+        postList.value = response.data.content;
+    } catch (error) {
+        console.error("게시글을 불러올 수 없습니다.", error);
+    }
+}
+
+
+// async function getPostList() {
+
+//     try {
+//         const token = localStorage.getItem('token');
+//         if (token) {
+//             axios.defaults.headers.common['Authorization'] = token;
+//             const response = await axios.post('http://localhost:5000/post/tab', {
+//                 tabRelationId: search.value.tabRelationId,
+//                 categoryId: search.value.categoryId,
+//                 title: search.value.title,
+//                 content: search.value.content,
+//                 tags: search.value.tags
+//             });
+//             postList.value = response.data.content;
+//         } else {
+//             alert("잘못된 접근입니다.");
+//         }
+//     } catch (error) {
+//         alert("게시글을 불러올 수 없습니다.");
+//     } finally {
+//     }
+// }
+
+const convertToDate = (date) => {
+    const dateSplit = date.split("T");
+    dateSplit[1] = dateSplit[1].split(".")[0];
+
+    return dateSplit[0] + " " + dateSplit[1];
+};
+
+</script>
+
+<style>
+.content-preview {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    /* 두 줄을 표시하고 넘어가면 말줄임표 표시 */
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    min-height: 50px;
+}
+
+#profile-img {
+    float: left;
+}
+
+.tag {
+    margin-left: 5px;
+}
+
+#top {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
