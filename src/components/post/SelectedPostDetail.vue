0f7bda6e4a2c49200de52216e4b33797a55596b0
hso <sh07202@naver.com>
2024-06-04 12:11:21 +0900

    feat(post) : 상세보기 뷰 수정

--- src/components/post/SelectedPostDetail.vue
+++ src/components/post/SelectedPostDetail.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="container">
+  <div v-if="post" id="container">
     <div id="top">
 
       <div id="title-info">
-        <h1 id="title"> {{ post.title }} </h1>
+        <h1 id="title"><strong>{{ post.title }}</strong></h1>
 
-        <p class="like" data-bs-toggle="dropdown" aria-expanded="false">
-          <span class="material-icons">favorite</span>
-          &nbsp;<span>{{ post.likeList.length }}</span>
-        </p>
-
+        <p id="like-container">
+          <span class="material-icons" id="favorite-icon" @click="favoritePost(post.originId ? post.originId : post.id)"
+            :style="{ color: post.isFavorite ? '#042444' : '#EFEFEF' }">bookmark</span>
+          &nbsp;&nbsp;<span class="material-icons" id="like-icon"
+            @click="likePost(post.originId ? post.originId : post.id)"
+            :style="{ color: post.isLike ? '#042444' : '#EFEFEF' }">favorite</span>
+        <div class="like" data-bs-toggle="dropdown" aria-expanded="false">
+          <span><strong>&nbsp;{{ post.likeList.length }}</strong></span>
+        </div>
         <ul class="dropdown-menu">
           <li v-for="like in post.likeList">
             <b-avatar variant="info" :src="like.profileImg ? like.profileImg : 'https://placekitten.com/300/300'">
@@ -17,6 +21,7 @@
             &nbsp;{{ like.name }}&nbsp;
           </li>
         </ul>
+        </p>
       </div>
 
       <div id="etc">
@@ -47,7 +52,7 @@
       <div id="post-info">
         <div>
           <p><strong>최종 수정일</strong></p>
-          <p>
+          <p id="date">
             <span>{{ convertToDate(post.createdAt) }} &nbsp;</span>
             <template v-if="!general">
               <span class="material-icons" data-bs-target="#historyModal" data-bs-toggle="modal">
@@ -62,7 +67,7 @@
           <div class="hashtag">
             <p>
               <span class="tag" v-for="tag in post.tags" :key="tag.id">
-                <b-badge>#{{ tag }}</b-badge>
+                <b-badge class="custom-badge" variant="custom-badge">#{{ tag }}</b-badge>
               </span>&nbsp;
             </p>
           </div>
@@ -70,7 +75,7 @@
         <hr>
         <div>
           <div class="authors" data-bs-toggle="collapse" :data-bs-target="`#authorList`" :aria-controls="`#authorList`">
-            <p><strong>참여자 ▽</strong></p>
+            <p id="author-drop"><strong>참여자</strong><span class="material-icons">arrow_drop_down</span></p>
           </div>
           <div class="show" id="authorList">
             <p class="author" v-for="participant in post.participants" :key="participant.id">
@@ -82,6 +87,8 @@
         </div>
       </div>
     </div>
+    
+    <span class="material-icons" id="top-btn" @click=scrollToTop()>assistant_navigation</span>
 
     <!-- 퀴즈 모달창 -->
     <div v-if="isQuizAvailable" id="quiz-container">
@@ -93,43 +100,53 @@
       <div class="modal-dialog modal-fullscreen">
         <div class="modal-content">
           <div class="modal-header">
-            <h3 class="" id="modalToggleLabel">변경 히스토리 확인</h3>
+            <h2 class="" id="modalToggleLabel"><strong>변경 히스토리 확인</strong></h2>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body" v-if="historyPost">
-            <div class="history" id="historyContent">
-              <h4 id="title"> {{ historyPost.title }} </h4>
-              <small> {{ convertToDate(historyPost.createdAt) }}</small>
-              <b-button variant="outline-dark" @click="restorePost()">버전 복원하기</b-button>
-              <div id="tag-div">
-                <div class="hashtag">
-                  <p>
-                    <span class="tag" v-for="tag in historyPost.tags" :key="tag.id">
-                      <b-badge>#{{ tag }}</b-badge>
-                    </span>&nbsp;
-                  </p>
+          <div class="modal-body">
+            <template v-if="historyPost">
+              <div class="history" id="historyContent">
+                <h4 id="title"> {{ historyPost.title }} </h4>
+                <div id="date-restore">
+                  <small> {{ convertToDate(historyPost.createdAt) }}</small>
+                &nbsp;<b-button size="sm" variant="outline-secondary" @click="restorePost()">버전 복원하기</b-button>
                 </div>
+                <div id="tag-div">
+                  <div class="hashtag">
+                    <p>
+                      <span class="tag" v-for="tag in historyPost.tags" :key="tag.id">
+                        <b-badge class="custom-badge" variant="custom-badge">#{{ tag }}</b-badge>
+                      </span>&nbsp;
+                    </p>
+                  </div>
+                </div>
+                <hr>
+                <div id="content" v-html="historyPost.content"></div>
               </div>
-              <div id="content" v-html="historyPost.content"></div>
-            </div>
-            <div class="history" id="historyList">
-              <b-list-group>
-                <template v-for="history in post.history" :key="history.id">
-                  <b-list-group-item button @click="setHistoryContent(history)">
-                    <div>
-                      <p>
-                      <h3>
-                        <b-avatar variant="info"
-                          :src="history.author.profileImg ? history.author.profileImg : '@/assets/images/profile_image.png'"></b-avatar>
-                        <span> &nbsp;{{ history.author.name }}&nbsp; </span>
-                      </h3>
-                      </p>
-                      <small>{{ convertToDate(history.createdAt) }}</small>
-                    </div>
-                  </b-list-group-item>
-                </template>
-              </b-list-group>
-            </div>
+              <div class="history" id="historyList">
+                <h3>버전 기록</h3>
+                <hr>
+                <b-list-group>
+                  <template v-for="history in post.history" :key="history.id">
+                    <b-list-group-item button @click="setHistoryContent(history)">
+                      <div>
+                        <p>
+                        <h5>
+                          <b-avatar variant="info"
+                            :src="history.author.profileImg ? history.author.profileImg : '@/assets/images/profile_image.png'"></b-avatar>
+                          <span> &nbsp;{{ history.author.name }}&nbsp; </span>
+                        </h5>
+                        </p>
+                        <small>{{ convertToDate(history.createdAt) }}</small>
+                      </div>
+                    </b-list-group-item>
+                  </template>
+                </b-list-group>
+              </div>
+            </template>
+            <template v-else>
+              <div><p>게시글 히스토리가 존재하지 않습니다.</p></div>
+            </template>
           </div>
         </div>
       </div>
@@ -151,167 +168,16 @@ const currentRoute = useRoute();
 const postId = currentRoute.params.id;
 let general = ref((currentRoute.path.includes("organization")));
 
-const isAuthorized = true;
+const isAuthorized = ref(false);
 const historyPost = ref(null);
 const selectedQuizId = ref(null);
 const isQuizAvailable = ref(false);
 
-const post = ref({
-  id: 2,
-  title: "자바의 기본 문법 수정\r\n",
-  content: `
-  <div id="study-content">
-    <h2>스터디 관련 내용</h2>
-    <p>저희 스터디는 다음과 같은 내용으로 진행됩니다:</p>
-    <ul>
-        <li>매주 목요일 저녁 7시에 Zoom을 통한 온라인 회의</li>
-        <li>각자 주제에 대한 조사와 발표 준비</li>
-        <li>마감 시간을 준수하여 발표 자료 공유</li>
-        <li>질의응답 및 토론 시간 확보</li>
-    </ul>
-    <p>스터디에 참여하기 위해서는 사전에 주제 선정 및 조사가 필요합니다. 또한, Zoom 링크와 발표 자료는 스터디 시작 1시간 전까지 공유되어야 합니다.</p>
-    <p>더 자세한 내용은 아래 연락처로 문의해 주세요:</p>
-    <ul>
-        <li>이메일: example@example.com</li>
-        <li>전화번호: 010-1234-5678</li>
-    </ul>
-</div>
-  `,
-  createdAt: "2021-11-08T11:44:30.327959",
-  author: {
-    id: 2,
-    email: "test1",
-    name: "테스트1",
-    profileImg: "test1.jpg",
-    role: "ROLE_NORMAL",
-    startDate: null,
-    endDate: null,
-    phoneNumber: null,
-    teamId: 2,
-    positionId: 2,
-    rankId: 2
-  },
-  originId: 1,
-  recentId: null,
-  tabRelationId: 1,
-  categoryId: null,
-  tags: ["개발", "tag1", "tag2", "tag6", "tag7", "tag8"],
-  history: [
-    {
-      id: 1,
-      title: "자바의 기본 문법",
-      content: "자바는 객체 지향 프로그래밍 언어로, 강력한 기능과 유연성을 제공합니다. 이번 스터디에서는 자바의 기본 문법과 객체 지향 프로그래밍의 개념을 학습합니다. 변수 선언, 자료형, 조건문, 반복문, 클래스와 객체, 상속과 다형성 등의 내용을 다룹니다.",
-      createdAt: "2021-11-08T11:44:30.327959",
-      author: {
-        id: 1,
-        email: "admin",
-        name: "관리자",
-        profileImg: null,
-        role: "ROLE_ADMIN",
-        startDate: "2024-05-17",
-        endDate: null,
-        phoneNumber: null,
-        teamId: 1,
-        positionId: 1,
-        rankId: 1
-      },
-      originId: null,
-      recentId: 2,
-      tabRelationId: 1,
-      categoryId: null,
-      tags: ["개발", "tag1", "tag2", "tag3", "tag4", "tag5"],
-      history: null,
-      participants: null
-    },
-    {
-      id: 2,
-      title: "자바의 기본 문법 수정\r\n",
-      content: "수정 내용",
-      createdAt: "2021-11-08T11:44:30.327959",
-      author: {
-        id: 2,
-        email: "test1",
-        name: "테스트1",
-        profileImg: "test1.jpg",
-        role: "ROLE_NORMAL",
-        startDate: null,
-        endDate: null,
-        phoneNumber: null,
-        teamId: 2,
-        positionId: 2,
-        rankId: 2
-      },
-      originId: 1,
-      recentId: null,
-      tabRelationId: 1,
-      categoryId: null,
-      tags: ["개발", "tag1", "tag2", "tag6", "tag7", "tag8"],
-      history: null,
-      participants: null
-    }
-  ],
-  participants: [
-    {
-      id: 1,
-      name: "홍길동",
-      email: "admin",
-      profileImg: "https://placekitten.com/300/300",
-      name: "관리자",
-      profileImg: null,
-      role: "ROLE_ADMIN",
-      startDate: "2024-05-17",
-      endDate: null,
-      phoneNumber: null,
-      teamId: 1,
-      positionId: 1,
-      rankId: 1
-    },
-    {
-      id: 2,
-      name: "김영희",
-      email: "test1",
-      profileImg: "https://placekitten.com/300/300",
-      role: "ROLE_NORMAL",
-      startDate: null,
-      endDate: null,
-      phoneNumber: null,
-      teamId: 2,
-      positionId: 2,
-      rankId: 2
-    }
-  ],
-  "likeList": [
-    {
-      "id": 1,
-      "email": "admin",
-      "name": "관리자",
-      "profileImg": null,
-      "role": "ROLE_ADMIN",
-      "startDate": "2024-05-17",
-      "endDate": null,
-      "phoneNumber": null,
-      "teamId": 1,
-      "positionId": 1,
-      "rankId": 1
-    },
-    {
-      "id": 2,
-      "email": "test1",
-      "name": "테스트1",
-      "profileImg": "test1.jpg",
-      "role": "ROLE_NORMAL",
-      "startDate": null,
-      "endDate": null,
-      "phoneNumber": null,
-      "teamId": 2,
-      "positionId": 2,
-      "rankId": 2
-    }
-  ]
-});
+const post = ref(null);
 
 
 onMounted(async () => {
+  await getPostById()
   await checkQuizVisibility();
   setHistoryContent(post.value.history[0]);
 });
@@ -333,6 +199,40 @@ const modifyPost = (postId) => {
   });
 };
 
+async function likePost(id) {
+  try {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+      const response = await axios.post(`http://localhost:5000/post/like`, { postId: id });
+
+      post.value.isLike = !post.value.isLike
+    } else {
+      alert("잘못된 접근입니다.");
+    }
+  } catch (error) {
+    console.log("오류가 발생했습니다.");
+  } finally {
+  }
+}
+
+async function favoritePost(id) {
+  try {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+      const response = await axios.post(`http://localhost:5000/post/favorite`, { postId: id });
+
+      post.value.isFavorite = !post.value.isFavorite
+    } else {
+      alert("잘못된 접근입니다.");
+    }
+  } catch (error) {
+    console.log("오류가 발생했습니다.");
+  } finally {
+  }
+}
+
 async function restorePost() {
   if (confirm("선택한 버전으로 복원하시겠습니까?")) {
     historyPost.value.id = null;
@@ -345,29 +245,6 @@ const setHistoryContent = (selectPost) => {
   historyPost.value = selectPost;
 };
 
-async function getAuthorized() {
-  const response = await axios.get(`http://localhost:5000/post/isAuthor/${originId}`);
-  isAuthorized.value = response.data;
-
-  const segments = currentRoute.path.split('/');
-  if (segments.length > 2 && segments[2] === "organization") {
-    general = true;
-  }
-
-  // try {
-  //     const token = localStorage.getItem('token');
-  //     if (token) {
-  //         axios.defaults.headers.common['Authorization'] = token;
-  // const response = await axios.get(`http://localhost:5000/tab/name/${tabId}`);
-  //         tabName.value = response.data;
-  //     } else {
-  //         alert("잘못된 접근입니다.");
-  //     }
-  // } catch (error) {
-  //     alert("탭 이름을 불러올 수 없습니다.");
-  // } finally {
-  // }
-}
 
 async function deletePost(postId) {
   try {
@@ -396,14 +273,20 @@ async function getPostById() {
     const token = localStorage.getItem('token');
     if (token) {
       axios.defaults.headers.common['Authorization'] = token;
-      const response = await axios.post(`http://localhost:5000/post/${postId}`);
+      const response = await axios.get(`http://localhost:5000/post/find/${postId}`);
       post.value = response.data;
-      isAuthorized.value = (await axios.post(`http://localhost:5000/post/isAuthor/${postId}`)).data;
+
+      isAuthorized.value = await axios.get(`http://localhost:5000/post/isAuthor/${postId}`).data;
+
+      const segments = currentRoute.path.split('/');
+      if (segments.length > 2 && segments[2] === "organization") {
+        general = true;
+      }
+
     } else {
       alert("잘못된 접근입니다.");
     }
   } catch (error) {
-    alert("게시글을 불러올 수 없습니다.");
   } finally {
   }
 }
@@ -460,7 +343,12 @@ const checkQuizVisibility = async () => {
   }
 };
 
-
+function scrollToTop(){
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
 </script>
 
 <style>
@@ -530,9 +418,14 @@ const checkQuizVisibility = async () => {
 
 #post-info {
   width: 25%;
+  padding-left: 10px;
 }
 
-#tag-div {
+#tag-div{
+  margin-top: 10px;
+}
+
+#date-restore{
   margin-top: 20px;
 }
 
@@ -561,9 +454,35 @@ li {
   margin: 5px;
 }
 
-.like{
-    display: flex;
-      align-items: center;
-      margin-top: 10px;
+#like-container {
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.material-icons:hover {
+  color: #042444;
+}
+
+#date, #author-drop{
+  display: flex;
+  align-items: center;
+}
+
+.custom-badge {
+  background-color: #042444;
+  color: white;
+}
+
+.history{
+  padding: 10px;
+}
+
+#top-btn {
+  font-size: 50px;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  color: #042444;
 }
 </style>
