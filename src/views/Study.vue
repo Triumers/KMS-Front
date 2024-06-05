c2033f38ed5f1ce4c1b87ee977deedbc986ad60a
hso <sh07202@naver.com>
2024-06-05 17:28:13 +0900

    feat(Study) : 스터디 여러개 표시 및 선택에 따라 변경

--- src/views/Study.vue
+++ src/views/Study.vue
@@ -1,14 +1,67 @@
 <template>
-    <div>
-      <h1>Study Page</h1>
-      <RouterView/>
+  <div>
+    <div id="top">
+      <span class="search-type">
+        <select v-model="selectedTab" class="form-select pt-1 search-type"
+                style="width:fit-content; vertical-align: middle; margin-right: 10px;" @change="changeStudy">
+          <option v-for="tab in studyTab" :value="tab.tabId">
+            {{ tab.bottomTabName }}
+          </option>
+        </select>
+      </span>
     </div>
-  </template>
-  
-  <script>
-import { RouterView } from "vue-router";
-  export default {
-    name: 'Study'
+    <div>
+      <RouterView :key="$route.fullPath" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const categoryList = ref(null);
+const studyTab = ref(null);
+const selectedTab = ref(null);
+
+const router = useRouter();
+
+onMounted(async () => {
+  await getUserCategory();
+});
+
+const changeStudy = () => {
+  const path = `/group/study/${selectedTab.value}`;
+  router.push(path);
+};
+
+async function getUserCategory() {
+  try {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+      const response = await axios.get(`http://localhost:5000/group/employee`);
+      categoryList.value = response.data;
+      setCategory();
+    } else {
+      console.log("잘못된 접근입니다.");
+    }
+  } catch (error) {
+    console.log("탭 이름을 불러올 수 없습니다.");
   }
-  </script>
-  
+}
+
+function setCategory() {
+  studyTab.value = categoryList.value.filter(category => category.topTabName === "스터디").map(category => category);
+  selectedTab.value = studyTab.value[0].tabId;
+}
+</script>
+
+<style>
+#top {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+}
+</style>
