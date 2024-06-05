50228a7050ccb200c789fac4195d0864fb54c647
noctesilente <mox9nox@gmail.com>
2024-06-05 20:39:04 +0900

    feat(user): 이미 로그인한 상태일 경우 파팝업창 뜨고 위키 글로 이동하게 수정

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,52 +1,63 @@
 <template>
-    <div class="container">
-      <div class="login-form">
-        <h2 class="form-title">로그인</h2>
-        <form @submit.prevent="login">
-          <div class="form-group">
-            <label for="email">이메일</label>
-            <input type="email" id="email" v-model="email" required class="form-input" />
-          </div>
-          <div class="form-group">
-            <label for="password">비밀번호</label>
-            <input type="password" id="password" v-model="password" required class="form-input" />
-          </div>
-          <button type="submit" class="login-button">로그인</button>
-        </form>
-      </div>
+  <div class="container">
+    <div class="login-form">
+      <h2 class="form-title">로그인</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">이메일</label>
+          <input type="email" id="email" v-model="email" required class="form-input" />
+        </div>
+        <div class="form-group">
+          <label for="password">비밀번호</label>
+          <input type="password" id="password" v-model="password" required class="form-input" />
+        </div>
+        <button type="submit" class="login-button">로그인</button>
+      </form>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import axios from 'axios';
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  const email = ref('');
-  const password = ref('');
-  
-  async function login() {
-    try {
-      const response = await axios.post('/login', {
-        email: email.value,
-        password: password.value,
-      }).then(response => {
-        localStorage.setItem('token', response.headers.get('Authorization'));
-        localStorage.setItem('role', response.headers.get('UserRole'));
-        router.push('/wiki/posts');
+  </div>
+</template>
 
-        return response.data;
-      })
-      // 로그인 성공 시 처리할 로직 추가
-      
-    } catch (error) {
-      console.error('Failed to login:', error);
-      // 로그인 실패 시 처리할 로직 추가
-      alert('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
-    }
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const email = ref('');
+const password = ref('');
+
+// 로그인 상태 확인 함수
+const checkLoginStatus = () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    alert('이미 로그인한 상태입니다.');
+    router.push('/wiki/posts');
   }
-  </script>
+};
+
+async function login() {
+  try {
+    const response = await axios.post('/login', {
+      email: email.value,
+      password: password.value,
+    }).then(response => {
+      localStorage.setItem('token', response.headers.get('Authorization'));
+      localStorage.setItem('role', response.headers.get('UserRole'));
+      router.push('/wiki/posts');
+      return response.data;
+    });
+    // 로그인 성공 시 처리할 로직 추가
+  } catch (error) {
+    console.error('Failed to login:', error);
+    // 로그인 실패 시 처리할 로직 추가
+    alert('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
+  }
+}
+
+onMounted(() => {
+  checkLoginStatus();
+});
+</script>
   
   <style scoped>
   .container {
