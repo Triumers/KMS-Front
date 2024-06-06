bb1f7327a2d57d75ce60ad0b83c9c63c6ff673a9
noctesilente <mox9nox@gmail.com>
2024-06-06 17:06:01 +0900

    refactor(manager): 코드 수정

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import router from './router/mainRouter.js';
 import App from './App.vue';
 import axios from 'axios';
 import store from './store/index.js';
+import { createStore } from 'vuex'; 
 
 axios.defaults.baseURL = 'http://localhost:5000'; // 백엔드 서버의 주소와 포트를 지정합니다.
 axios.defaults.withCredentials = true; // 쿠키를 포함하여 요청을 보내도록 설정합니다.
@@ -20,6 +21,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
 const app = createApp(App);
+
 app.use(router);
 app.use(store);
 app.use(BootstrapVue3);
