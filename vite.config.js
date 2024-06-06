5db21c59e497f93bb2cc10fbb60b06d7a11f1208
noctesilente <mox9nox@gmail.com>
2024-06-06 16:00:36 +0900

    refactor(manager): vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
+// vite.config.js
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    rollupOptions: {
-      external: ['vuex']
-    }
   }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['vuex']
+  //   }
+  // }
 });
