8ae6c3d255aad18ef627689cd9cabb18b0e9acb9
noctesilente <mox9nox@gmail.com>
2024-06-06 15:23:38 +0900

    refactor(manager): vuex 관련 코드 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +11,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['vuex']
+    }
   }
-})
+});
