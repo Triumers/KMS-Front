1ed472298ab3c216971a01bbbd70b84bc903d7fb
moomint8 <office05053@gmail.com>
2024-06-07 03:56:04 +0900

    feat(auth): 초기 비밀번호 변경 시 환영 페이지 리다이렉트 구현
    초기 비밀번호를 변경하는 경우 환영 페이지로 리다이렉트하는 기능이 구현되었습니다.

---
+++ src/components/user/WelcomeToJoin.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="congrats-message">
+            <img src="@/assets/images/pinkFlowerLeft.gif" alt="Left Flower" class="flower-image left-flower">
+            <img src="@/assets/images/pinkFlowerLeft.gif" alt="Left Flower" class="flower-image left-flower">
+            <img src="@/assets/images/pinkFlowerRight.gif" alt="Right Flower" class="flower-image right-flower">
+            <img src="@/assets/images/pinkFlowerRight.gif" alt="Right Flower" class="flower-image right-flower">
+        </div>
+        <div class="congrats-message">
+            <img src="@/assets/images/pinkFlowerLeft.gif" alt="Left Flower" class="flower-image left-flower">
+            <h2>귀하의 입사를 축하합니다 !</h2>
+            <img src="@/assets/images/pinkFlowerRight.gif" alt="Right Flower" class="flower-image right-flower">
+        </div>
+        <div class="congrats-message">
+            <img src="@/assets/images/pinkFlowerLeft.gif" alt="Left Flower" class="flower-image left-flower">
+            <img src="@/assets/images/pinkFlowerLeft.gif" alt="Left Flower" class="flower-image left-flower">
+            <img src="@/assets/images/pinkFlowerRight.gif" alt="Right Flower" class="flower-image right-flower">
+            <img src="@/assets/images/pinkFlowerRight.gif" alt="Right Flower" class="flower-image right-flower">
+        </div>
+    </div>
+</template>
+
+<script setup>
+  </script>
+  
+  <style scoped>
+    .template {
+        background-color: #f0f0f0;
+    }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 40px);
+    text-align: center;
+  }
+  
+  .congrats-message {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  h2 {
+    font-size: 40px;
+    font-weight: bold;
+    margin: 0 20px;
+  }
+  
+  .flower-image {
+  }
+  
+  .left-flower {
+    margin-right: 20px;
+  }
+  
+  .right-flower {
+    margin-left: 20px;
+  }
+  </style>
