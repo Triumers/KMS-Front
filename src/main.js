1dfe514e4b3b5c35674de9950fb1e20e124b7607
noctesilente <mox9nox@gmail.com>
2024-06-06 17:15:20 +0900

    refactor(manager): 코드 수정

--- src/main.js
+++ src/main.js
@@ -22,9 +22,7 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
 const app = createApp(App);
 
-app.use(new window.Vuex.Store(store))
-
 app.use(router);
-app.use(store);
+app.use(new window.Vuex.Store(store))
 app.use(BootstrapVue3);
 app.mount('#app');
