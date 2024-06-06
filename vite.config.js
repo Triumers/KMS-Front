0d776024aa1f4f6e5d3cf54b3a8ca65f9a701941
noctesilente <mox9nox@gmail.com>
2024-06-06 16:02:14 +0900

    refactor(manager): vite.config.js 및 .env 수정

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-// vite.config.js
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: env.PROD ? ['vuex'] : []
+      }
     }
-  }
-  // build: {
-  //   rollupOptions: {
-  //     external: ['vuex']
-  //   }
-  // }
-});
+  });
+};
